5338c51ee9c6726a3efc6e85d80927e9ad069dc0
Patrick Ubong Kingsley <47539464+bougzy@users.noreply.github.com>
2024-03-19 13:01:15 +0100

    Update AppNavbar.jsx

--- client/src/AppNavbar.jsx
+++ client/src/AppNavbar.jsx
@@ -1,34 +1,3 @@
-// import React from 'react';
-// import { Navbar, Nav, Container } from 'react-bootstrap';
-
-// const AppNavbar = ({ isLoggedIn }) => {
-//   return (
-//     <Navbar bg="dark" variant="dark" expand="lg">
-//       <Container>
-//         <Navbar.Brand href="#home" className="text-black bg-warning p-4 fw-bold rounded">VIRGO</Navbar.Brand>
-//         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-//         <Navbar.Collapse id="basic-navbar-nav">
-//           <Nav className="ml-auto">
-//             <Nav.Link href="/">Home</Nav.Link>
-//             {/* <Nav.Link href="/dashboard">Dashboard</Nav.Link> */}
-//             {/* <Nav.Link href="/coinmodal">Dashboard One</Nav.Link> */}
-//             {!isLoggedIn ? (
-//               <>
-//                 <Nav.Link href="/login" className="me-2">Login</Nav.Link>
-//                 <Nav.Link href="/register">Register</Nav.Link>
-//               </>
-//             ) : null}
-//             {/* <Nav.Link href="/my">My</Nav.Link> */}
-//           </Nav>
-//         </Navbar.Collapse>
-//       </Container>
-//     </Navbar>
-//   );
-// };
-
-// export default AppNavbar;
-
-
 import React from 'react';
 import { Navbar, Nav, Container, Button } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
