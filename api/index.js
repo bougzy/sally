13100d34387628503dc09adba4740a34b97465dd
bougzy <yubeespecial@gmail.com>
2024-03-19 03:46:24 +0100

    added some comments

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ const path = require('path');
 mongoose.connect('mongodb+srv://sally:sally@sally.xmod3df.mongodb.net/sally');
 
 app.use(cors({
-    origin: 'sally-frontend.vercel.app', // Replace with your frontend origin
+    origin: 'https://vercel.com/patrickspecials-projects/sally-frontend', // Replace with your frontend origin
     credentials: true, // Allow cookies and credentials
   }));
   
@@ -19,32 +19,32 @@ app.use('/api/users', require('./routes/usersRoutes'));
 
 
 // Set up multer for handling file uploads
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/'); // Destination folder for uploaded files
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-    cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
-  },
-});
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, 'uploads/'); // Destination folder for uploaded files
+//   },
+//   filename: function (req, file, cb) {
+//     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+//     cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+//   },
+// });
 
-const upload = multer({ storage: storage });
+// const upload = multer({ storage: storage });
 
 // Express route for file upload
-app.post('/upload', upload.single('file'), (req, res) => {
-  try {
-    // Access the uploaded file information using req.file
-    const { filename, path, size } = req.file;
-    console.log('File uploaded:', filename);
+// app.post('/upload', upload.single('file'), (req, res) => {
+//   try {
+//     // Access the uploaded file information using req.file
+//     const { filename, path, size } = req.file;
+//     console.log('File uploaded:', filename);
 
-    // You can save the file information to a database or return it in the response
-    res.json({ filename, path, size });
-  } catch (error) {
-    console.error('Error uploading file:', error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
+//     // You can save the file information to a database or return it in the response
+//     res.json({ filename, path, size });
+//   } catch (error) {
+//     console.error('Error uploading file:', error);
+//     res.status(500).json({ error: 'Internal server error' });
+//   }
+// });
 
 
 // Route for greeting message
